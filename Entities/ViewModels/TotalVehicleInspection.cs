2e937f743e14a27a58c7e79d3b690f68bf8c4dd4
Hiếu <anhhieuk50@gmail.com>
2026-02-05 11:47:52 +0700

    update

---
+++ Entities/ViewModels/TotalVehicleInspection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ViewModels
+{
+    public class TotalVehicleInspection
+    {
+        public double TotalCar { get; set; }//Tổng số xe theo ngày
+        public double TotalCarCompleted { get; set; }//Tổng số xe đã lấy hàng
+        public double TotalCarUnfinished { get; set; }//Tổng số xe chưa lấy hàng
+        public double TotalCarArriving16h { get; set; }//Tổng số xe đến lấy hàng sau 16h
+        public double TotalTroughType3 { get; set; }//Tổng số bản ghi theo từng loại máng
+        public double TotalTroughType4 { get; set; }
+        public double TotalTroughType5 { get; set; }
+        public double TotalTroughType6 { get; set; }
+        public double TotalTroughType7 { get; set; }
+        public double TotalTroughType8 { get; set; }
+        public double TotalWeightTroughType { get; set; }//TÍNH TỔNG TRỌNG LƯỢNG
+        public double TotalWeightTroughType3 { get; set; }
+        public double TotalWeightTroughType4 { get; set; }
+        public double TotalWeightTroughType5 { get; set; }
+        public double TotalWeightTroughType6 { get; set; }
+        public double TotalWeightTroughType7 { get; set; }
+        public double TotalWeightTroughType8 { get; set; }
+        public double TotalTimeInHour { get; set; }
+        public double AvgTimePerCompletedCar_Hour { get; set; }
+        public DateTime MaxVehicleTimeOutToday { get; set; }
+        public double TotalTimeWorkInHour { get; set; }
+        public double AverageProductivity { get; set; }
+        public DateTime VehicleWeighingTimeComplete { get; set; }
+
+    }
+}
---
+++ WEB.CMS/wwwroot/modules/report.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+    _report.init();
+});
+var _report = {
+    init: function () {
+        var newdate = new Date();
+        newdate.setDate(01);
+        var FromDateStr = newdate.toLocaleDateString("en-GB");
+        var ToDateStr = new Date().toLocaleDateString("en-GB");
+        _report.GetTotalVehicleInspection(FromDateStr, ToDateStr);
+        _report.GetSummaryVehicleBySite(FromDateStr, ToDateStr);
+    },
+    GetTotalVehicleInspection: function (fromdate, todate) {
+        $.ajax({
+            url: '/Report/TotalVehicleInspection',
+            type: 'POST',
+            data: {
+                fromdate: fromdate,
+                todate: todate
+            },
+            success: function (result) {
+                if (result.status == 0) {
+                    var html = `<div class="summary-card">
+                                    <div class="card-label">Tổng Sản lượng (Tấn)</div>
+                                    <div class="card-value">
+                                     ${_global_function.Comma(result.data.totalWeightTroughType)}
+                                    </div>
+                                </div>
+                                <div class="summary-card">
+                                    <div class="card-label">Tổng số Chuyến</div>
+                                    <div class="card-value">
+                                     ${_global_function.Comma(result.data.totalCar)}
+                                    </div></div>`;
+                    $('#summary-grid').html(html)
+
+                } 
+            }
+        });
+    },
+    GetSummaryVehicleBySite: function (fromdate, todate) {
+        $.ajax({
+            url: '/Report/GetSummaryVehicleBySite',
+            type: 'POST',
+            data: {
+                fromdate: fromdate,
+                todate: todate
+            },
+            success: function (result) {
+           
+                $('#table-data').html(result)
+
+                
+            }
+        });
+    },
+};
